2222087556e46ef98e32f24771501ed08a3280cc
Matthew Bekkema <mat8913ftw@gmail.com>
2014-04-11 15:43:55 +1000

    xmonad.hs enable smart borders

--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,15 @@
 import XMonad
+import XMonad.Layout.NoBorders
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+varLayout = layoutHook defaultConfig
+
 main = do
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , layoutHook = avoidStruts $ smartBorders varLayout
         }
