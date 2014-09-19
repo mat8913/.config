ef1062805f6cda5e1314c5615f22944ed26ff684
Matthew Bekkema <mbekkema97@gmail.com>
2014-09-20 00:09:33 +1000

    xmonad.hs: Use a subLayout

--- xmonad.hs
+++ xmonad.hs
@@ -5,14 +5,26 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Layout.FixedColumn
+import XMonad.Layout.BoringWindows
+import XMonad.Layout.SubLayouts
 import System.IO
 
-varLayout = layoutHook defaultConfig ||| FixedColumn 1 1 85 10
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+
+varLayout = layoutHook defaultConfig ||| subLayout [1,0] (Full) (FixedColumn 1 1 85 10)
+varKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList [
+   ((modm .|. controlMask, xK_m), withFocused (sendMessage . MergeAll))
+ , ((modm .|. controlMask, xK_u), withFocused (sendMessage . UnMerge))
+ , ((modm, xK_j), onGroup W.focusDown')
+ , ((modm, xK_k), onGroup W.focusUp')
+ , ((modm, xK_Tab), focusDown) ]
 
 main = do
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
         , focusedBorderColor = "#268AD2"
         , normalBorderColor = "#002B36"
-        , layoutHook = smartBorders ( avoidStruts varLayout ||| Full )
+        , layoutHook = boringWindows $ smartBorders ( avoidStruts varLayout ||| Full )
+        , keys = varKeys <+> keys defaultConfig
         }
