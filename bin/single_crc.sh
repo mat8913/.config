4d08bf4b1c505586ff6ba9bae08228d52923762e
Matthew Bekkema <mbekkema97@gmail.com>
2014-09-21 19:20:51 +1000

    single_crc.sh: Allow case insensitive checksums
    Converts checksum in filename to upper case before comparing it to
    actual checksum.

--- bin/single_crc.sh
+++ bin/single_crc.sh
@@ -7,7 +7,7 @@ fi
 for i in "$@"; do\
 	WANTED_CRC=$(echo "$i" | grep -o -E '[a-fA-F0-9]{8}' | tail -n 1)
 	ACTUAL_CRC=$(cksfv "$i" | tail -c 9)
-	if [ x"$WANTED_CRC" != x"$ACTUAL_CRC" ]; then
+	if [ x"${WANTED_CRC^^}" != x"$ACTUAL_CRC" ]; then
 		echo $i shall not pass
 	fi
 done
