e9d986bf0b6249abd296ec277754bb82ed6edcfe
Matthew Bekkema <mbekkema97@gmail.com>
2014-11-29 18:32:59 +1100

    bin/all_crc.sh: default to current directory.

--- bin/all_crc.sh
+++ bin/all_crc.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
+if [ $# -gt 1 ]; then
 	echo "Invalid usage."
 	exit 1
 fi
 
-find "$1" -type f -regextype posix-egrep -regex '.*[A-Fa-f0-9]{8}[^/]*$' \
+SPATH=${1:-.}
+
+find "$SPATH" -type f -regextype posix-egrep -regex '.*[A-Fa-f0-9]{8}[^/]*$' \
      -exec single_crc.sh {} \;
