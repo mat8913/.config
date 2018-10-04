e95a3e5f0a25e6e8d3657156ceeabca016294a5a
Matthew Harm Bekkema <mbekkema97@gmail.com>
2018-10-05 03:24:35 +1000

    Set GNOME desktop background

--- dconf/immutable/load.sh
+++ dconf/immutable/load.sh
@@ -2,3 +2,4 @@
 dconf load /org/gnome/terminal/ < "${XDG_CONFIG_HOME:-$HOME/.config}"/dconf/immutable/terminal.conf
 dconf load /org/gnome/settings-daemon/plugins/color/ < "${XDG_CONFIG_HOME:-$HOME/.config}"/dconf/immutable/nightlight.conf
 dconf load /org/gnome/desktop/wm/keybindings/ < "${XDG_CONFIG_HOME:-$HOME/.config}"/dconf/immutable/keybindings.conf
+gsettings set org.gnome.desktop.background picture-uri "${XDG_CONFIG_HOME:-$HOME/.config}"/wallpaper
