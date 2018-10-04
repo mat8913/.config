82f067128e78cea75e4e933b9eac1dd69a6c8017
Matthew Harm Bekkema <mbekkema97@gmail.com>
2018-10-04 19:50:21 +1000

    Add GNOME keybindings

--- dconf/immutable/load.sh
+++ dconf/immutable/load.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 dconf load /org/gnome/terminal/ < "${XDG_CONFIG_HOME:-$HOME/.config}"/dconf/immutable/terminal.conf
 dconf load /org/gnome/settings-daemon/plugins/color/ < "${XDG_CONFIG_HOME:-$HOME/.config}"/dconf/immutable/nightlight.conf
+dconf load /org/gnome/desktop/wm/keybindings/ < "${XDG_CONFIG_HOME:-$HOME/.config}"/dconf/immutable/keybindings.conf
