d380c82e5b0834481be78ea4896f5e0d49cff28a
Matthew Harm Bekkema <mbekkema97@gmail.com>
2018-10-04 19:44:25 +1000

    Add GNOME nightlight config

--- dconf/immutable/load.sh
+++ dconf/immutable/load.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 dconf load /org/gnome/terminal/ < "${XDG_CONFIG_HOME:-$HOME/.config}"/dconf/immutable/terminal.conf
+dconf load /org/gnome/settings-daemon/plugins/color/ < "${XDG_CONFIG_HOME:-$HOME/.config}"/dconf/immutable/nightlight.conf
